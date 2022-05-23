a54fa8674926ec118865e3eb09aa89d5e54c28b8
brai0 <98900545+brai0@users.noreply.github.com>
2022-05-23 12:35:36 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("LifeCycle", "onCreate invoked");
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("LifeCycle","onStart invoked");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("LifeCycle","onPause invoked");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("LifeCycle","onRestart invoked");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("LifeCycle","onResume invoked");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("LIfeCycle","onStop invoked");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Lifecycle","OnDestroy Invoked");
+    }
+}
