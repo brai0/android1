51aaaf06b7ff10289537d374af2a9d72103afe71
brai0 <98900545+brai0@users.noreply.github.com>
2022-08-03 10:42:21 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,61 +1,30 @@
-package com.example.firstapp;
+<?xml version="1.0" encoding="utf-8"?>
+<androidx.constraintlayout.widget.ConstraintLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    tools:context=".MainActivity">
 
-import androidx.appcompat.app.AppCompatActivity;
+    <TextView
+        android:id="@+id/text1"
+        android:layout_width="168dp"
+        android:layout_height="31dp"
+        android:text="@string/hello_there"
+        android:textSize="25dp"
+        app:layout_constraintBottom_toBottomOf="parent"
+        app:layout_constraintLeft_toLeftOf="parent"
+        app:layout_constraintRight_toRightOf="parent"
+        app:layout_constraintTop_toTopOf="parent" />
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
+    <Button
+        android:id="@+id/button"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:text="Button"
+        tools:layout_editor_absoluteX="143dp"
+        tools:layout_editor_absoluteY="395dp"
+        tools:ignore="MissingConstraints" />
 
-public class MainActivity extends AppCompatActivity {
 
-    @SuppressLint("SetTextI18n")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.d("LifeCycle", "onCreate invoked");
-        findViewById(R.id.button).setOnClickListener(v -> {
-            Intent intent= new Intent(MainActivity.this,SecondActivity.class);
-            Toast.makeText(this, "New Activity", Toast.LENGTH_SHORT).show();
-        });
-
-    }
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("LifeCycle","onStart invoked");
-
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("LifeCycle","onPause invoked");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d("LifeCycle","onRestart invoked");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("LifeCycle","onResume invoked");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("LIfeCycle","onStop invoked");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("Lifecycle","OnDestroy Invoked");
-    }
-}
+</androidx.constraintlayout.widget.ConstraintLayout>
